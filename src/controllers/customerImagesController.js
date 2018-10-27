4b5a81a87de4915db51169bb549f46c9dd737d59
Luis Felipe Tavera Orozco <lftavera@hotmail.com>
2018-10-27 16:52:26 -0500

    ltaverao95

--- src/controllers/customerImagesController.js
+++ src/controllers/customerImagesController.js
@@ -1,12 +1,38 @@
+const pathFolder = "./src/assets/";
+const fs = require('fs');
 const controller = {};
 
 controller.list = (req, res) => {
-    res.render('customerImages', {data: ''});
+
+    res.render('customerImages', { data: '' });
 };
 
 controller.getByName = (req, res) => {
     const name = req.body.name;
-    res.render('customerImages', {data: 'https://www.youtube.com/yts/img/yt_1200-vfl4C3T0K.png'});
+
+    if(!name){
+        res.render('customerImages', { data: '' });
+        return;
+    }
+
+    fs.readdir(pathFolder, (err, files) => {
+
+        files.forEach(file => {
+
+            let currentFileWithoutExtension = file.split('.')[0];
+            if(currentFileWithoutExtension == name){
+                fs.readFile(pathFolder + file, function (err, data) {
+                    if (err) throw err;
+        
+                    // Encode to base64
+                    var encodedImage = new Buffer(data, 'binary').toString('base64');
+        
+                    res.render('customerImages', { data: encodedImage });
+                });
+            }
+        });
+
+    })
 };
 
 module.exports = controller;
